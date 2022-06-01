e4f7413ce8cdd87ae65c1a9b3be2ff9d82ef9004
Diego Viana <dsantos@inf.ufsm.br>
2022-06-01 18:24:27 -0300

    add rel in a

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -11,7 +11,7 @@ const CreateMaze = () => {
     <div className={styles.create_maze}>
       <h2>Criar jogo</h2>
       <p>Ao clicar no botão abaixo você será redirecionado para o página de criação do jogo.</p>
-      <a className="btn" target="_blank" href={"https://mapper-iota.vercel.app/index.html?uid=" + uid}>Ir</a>
+      <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mapper-iota.vercel.app/index.html?uid=" + uid}>Ir</a>
       {/*<a className="btn" href="https://mapper-iota.vercel.app/index.html?esteDeenvio">Ir</a>*/}
     </div>
   )
