f9689d4d1e66f9bb3e78bc535d58535c4d6c7b9c
Diego Viana <dsantos@inf.ufsm.br>
2022-06-02 14:49:46 -0300

    correção do texto

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -10,7 +10,7 @@ const CreateMaze = () => {
   return (
     <div className={styles.create_maze}>
       <h2>Criar jogo</h2>
-      <p>Ao clicar no botão abaixo você será redirecionado para o página de criação do jogo.</p>
+      <p>Ao clicar no botão abaixo você será redirecionado para a página de criação do jogo.</p>
       <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mapper-iota.vercel.app/index.html?uid=" + uid}>Ir</a>
       {/*<a className="btn" href="https://mapper-iota.vercel.app/index.html?esteDeenvio">Ir</a>*/}
     </div>
