70d6002911eb5ef82051180f2d806eb17f1e5d31
Diego Viana <dsantos@inf.ufsm.br>
2022-06-02 23:06:40 -0300

    update url maze builder

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -11,8 +11,8 @@ const CreateMaze = () => {
     <div className={styles.create_maze}>
       <h2>Criar jogo</h2>
       <p>Ao clicar no botão abaixo você será redirecionado para a página de criação do jogo.</p>
-      <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mapper-iota.vercel.app/index.html?uid=" + uid}>Ir</a>
-      {/*<a className="btn" href="https://mapper-iota.vercel.app/index.html?esteDeenvio">Ir</a>*/}
+      <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mazegamebuilder.vercel.app/index.html?uid=" + uid}>Ir</a>
+      {/*<a className="btn" href="https://mazegamebuilder.vercel.app/index.html?esteDeenvio">Ir</a>*/}
     </div>
   )
 }
