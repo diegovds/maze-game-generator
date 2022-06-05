24b28485f56755253e6451d0b896d12f0d6e52b7
Diego Viana <dsantos@inf.ufsm.br>
2022-06-04 22:05:40 -0300

    (createMaze) busca o id do uid no bd

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -1,17 +1,37 @@
 import styles from './CreateMaze.module.css'
 
-import { useState } from 'react'
 import { useAuthValue } from "../../context/AuthContext"
+import { useEffect, useState } from 'react'
 
 const CreateMaze = () => {
   const {user} = useAuthValue()
   const uid = user.uid
 
+  const [userId, setUserId] = useState([])
+
+  useEffect(() => {
+    const searchUserId = async () => {
+      const response = await fetch("https://maze-game-backend.herokuapp.com/api/users")
+      var data = await response.json()
+  
+      for(var users in data){
+        for(var user in data[users]){
+          if(data[users][user].uid === uid){
+            setUserId(data[users][user].id)
+            console.log(data[users][user].id)
+          }
+        }
+      }
+    };
+    searchUserId()
+  }, [uid])
+  
+
   return (
     <div className={styles.create_maze}>
       <h2>Criar jogo</h2>
       <p>Ao clicar no botão abaixo você será redirecionado para a página de criação do jogo.</p>
-      <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mazegamebuilder.vercel.app/index.html?uid=" + uid}>Ir para o Maze Builder</a>
+      <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mazegamebuilder.vercel.app/index.html?userId=" + userId}>Ir para o Maze Builder</a>
       {/*<a className="btn" href="https://mazegamebuilder.vercel.app/index.html?esteDeenvio">Ir</a>*/}
     </div>
   )
