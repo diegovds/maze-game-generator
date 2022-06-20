130abfd8b2f94bdfe03edd80546bcc35794ed1f6
Diego Viana <dsantos@inf.ufsm.br>
2022-06-20 11:00:23 -0300

    add background CT

--- src/components/MazeDetail.js
+++ src/components/MazeDetail.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const MazeDetail = ({ maze }) => {
   return (
     <div className={styles.maze}>
-      <img src="/sky.png" alt="Céu com estrelas" />
+      <img src="/background.png" alt="Centro de Tecnologia (CT) é uma unidade da Universidade Federal de Santa Maria" />
       <h3>{maze.name}</h3>
       <p id='date'>Criado em: {maze.created_at}</p>
       <Link to={`/mazes/${maze.id}`} className='btn'>Detalhes</Link>
