2124e322d73a127853c5c99d7293cd3a6f47dd4c
Diego Viana <dsantos@inf.ufsm.br>
2022-07-12 14:50:07 -0300

    correção do centro da tela pt.2

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -32,40 +32,42 @@ const Login = () => {
   }, [authError])
 
   return (
-    <div className={styles.login}>
-      <h1>Entrar</h1>
-      <p>Faça o login para poder utilizar o sistema</p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>E-mail:</span>
-          <input
-            type="email"
-            name="email"
-            required
-            placeholder="E-mail do usuário"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>Senha:</span>
-          <input
-            type="password"
-            name="password"
-            required
-            placeholder="Insira sua senha"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </label>
-        {!loading && <button className="btn">Entrar</button>}
-        {loading && (
-          <button className="btn" disabled>
-            Aguarde...
-          </button>
-        )}
-        {error && <p className="error">{error}</p>}
-      </form>
+    <div className={styles.container}>
+      <div className={styles.login}>
+        <h1>Entrar</h1>
+        <p>Faça o login para poder utilizar o sistema</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>E-mail:</span>
+            <input
+              type="email"
+              name="email"
+              required
+              placeholder="E-mail do usuário"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Senha:</span>
+            <input
+              type="password"
+              name="password"
+              required
+              placeholder="Insira sua senha"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </label>
+          {!loading && <button className="btn">Entrar</button>}
+          {loading && (
+            <button className="btn" disabled>
+              Aguarde...
+            </button>
+          )}
+          {error && <p className="error">{error}</p>}
+        </form>
+      </div>
     </div>
   )
 }
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -40,62 +40,64 @@ const Register = () => {
   }, [authError])
 
   return (
-    <div className={styles.register}>
-      <h1>Cadastre-se para criar um jogo</h1>
-      <p>Crie seu usuário</p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>Nome:</span>
-          <input
-            type="text"
-            name="displayName"
-            required
-            placeholder="Nome do usuário"
-            value={displayName}
-            onChange={e => setDisplayName(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>E-mail:</span>
-          <input
-            type="email"
-            name="email"
-            required
-            placeholder="E-mail do usuário"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>Senha:</span>
-          <input
-            type="password"
-            name="password"
-            required
-            placeholder="Insira sua senha"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>Confirmação de senha:</span>
-          <input
-            type="password"
-            name="confirmPassword"
-            required
-            placeholder="Confirme a sua senha"
-            value={confirmPassword}
-            onChange={e => setConfirmPassword(e.target.value)}
-          />
-        </label>
-        {!loading && <button className="btn">Cadastrar</button>}
-        {loading && (
-          <button className="btn" disabled>
-            Aguarde...
-          </button>
-        )}
-        {error && <p className="error">{error}</p>}
-      </form>
+    <div className={styles.container}>
+      <div className={styles.register}>
+        <h1>Cadastre-se para criar um jogo</h1>
+        <p>Crie seu usuário</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome:</span>
+            <input
+              type="text"
+              name="displayName"
+              required
+              placeholder="Nome do usuário"
+              value={displayName}
+              onChange={e => setDisplayName(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>E-mail:</span>
+            <input
+              type="email"
+              name="email"
+              required
+              placeholder="E-mail do usuário"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Senha:</span>
+            <input
+              type="password"
+              name="password"
+              required
+              placeholder="Insira sua senha"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Confirmação de senha:</span>
+            <input
+              type="password"
+              name="confirmPassword"
+              required
+              placeholder="Confirme a sua senha"
+              value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)}
+            />
+          </label>
+          {!loading && <button className="btn">Cadastrar</button>}
+          {loading && (
+            <button className="btn" disabled>
+              Aguarde...
+            </button>
+          )}
+          {error && <p className="error">{error}</p>}
+        </form>
+      </div>
     </div>
   )
 }
