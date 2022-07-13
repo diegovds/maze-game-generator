8bf15f59938de982f0f14cd1d044e98e1bed2c75
Diego Viana <dsantos@inf.ufsm.br>
2022-07-13 01:21:36 -0300

    card 404

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -3,13 +3,15 @@ import styles from "./NotFound.module.css";
 const NotFound = () => {
   return (
     <div className={styles.container}>
-      <h2>Página não encontrada!</h2>
-      <>
-        <p className={styles.p_a}>
-          Ocorreu um erro ao procurar o jogo
-        </p>
-        {/*<Link to="/" className="btn btn-dark">Voltar</Link>*/}
-      </>
+      <div className={styles.info}>
+        <h1>Erro 404</h1>
+        <br />
+        <h2>A página que você procura não foi encontrada</h2>
+        <>
+          {/*<p className={styles.p_a}>Ocorreu um erro ao procurar o jogo</p>
+          <Link to="/" className="btn btn-dark">Voltar</Link>*/}
+        </>
+      </div>
     </div>
   );
 };
