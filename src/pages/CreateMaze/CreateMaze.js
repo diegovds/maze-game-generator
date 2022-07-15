59e99591470d974a9f7c6cbb58ea55cd23e0c920
Diego Viana <dsantos@inf.ufsm.br>
2022-07-15 16:59:40 -0300

    Revert "useFetch hook in create maze page"
    This reverts commit b563a4f83536cc0e0f010e771be97e2cbd718140.

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -1,40 +1,34 @@
-import styles from "./CreateMaze.module.css";
+import styles from './CreateMaze.module.css'
 
-import { backend } from "../../backend/config";
-
-// hooks
-import useFetch from "react-fetch-hook";
-import { useEffect, useState } from "react";
-import { useAuthValue } from "../../context/AuthContext";
+import { backend } from '../../backend/config'
+import { useAuthValue } from "../../context/AuthContext"
+import { useEffect, useState } from 'react'
 
 const CreateMaze = () => {
-  const { user } = useAuthValue();
-  const uid = user.uid;
+  const {user} = useAuthValue()
+  const uid = user.uid
 
-  const [userId, setUserId] = useState(undefined);
+  const [userId, setUserId] = useState(undefined)
 
-  const { isLoading, error, data } = useFetch(backend + "/users");
-
-  const loadingUser = userId === undefined;
-
-  console.log("error", error);
-  console.log("isloading", isLoading);
+  const loadingUser = userId === undefined
 
   useEffect(() => {
     const searchUserId = async () => {
-      if (!isLoading && !error) {
-        data.data.forEach((item) => {
-          if (item.uid === uid) {
-            setUserId(item.id);
-            /*console.log(data[user].id)*/
-          }
-        });
+      const response = await fetch(backend + "/users")
+      var data = await response.json()
+      data = data.data
+  
+      for(var user in data){
+        if(data[user].uid === uid){
+          setUserId(data[user].id)
+          /*console.log(data[user].id)*/
+        }
       }
     };
-    searchUserId();
-  }, [uid, data, isLoading, error]);
-
-  if (isLoading && loadingUser) {
+    searchUserId()
+  }, [uid])
+  
+  if (loadingUser) {
     return (
       <div className="loading">
         <div className="dual-ring"></div>
@@ -42,39 +36,19 @@ const CreateMaze = () => {
           <p>Carregando...</p>
         </div>
       </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="loading">
-        <p>Ocorreu um erro, por favor tente mais tarde.</p>
-      </div>
-    );
+    )
   }
 
   return (
     <div className={styles.maze_container}>
       <div className={styles.create_maze}>
         <h2>Criar jogo</h2>
-        <p>
-          Ao clicar no botão abaixo você será redirecionado para a página de
-          criação do jogo.
-        </p>
-        <a
-          className="btn"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={
-            "https://mazegamebuilder.vercel.app/index.html?userId=" + userId
-          }
-        >
-          Ir para o Maze Builder
-        </a>
+        <p>Ao clicar no botão abaixo você será redirecionado para a página de criação do jogo.</p>
+        <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mazegamebuilder.vercel.app/index.html?userId=" + userId}>Ir para o Maze Builder</a>
         {/*<a className="btn" href="https://mazegamebuilder.vercel.app/index.html?esteDeenvio">Ir</a>*/}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CreateMaze;
+export default CreateMaze
