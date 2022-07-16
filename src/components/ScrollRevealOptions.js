6ed42fef7bc728a0c11825f3f9e74ce2e01b8f96
Diego Viana <dsantos@inf.ufsm.br>
2022-07-15 22:52:26 -0300

    Update ScrollRevealOptions.js

--- src/components/ScrollRevealOptions.js
+++ src/components/ScrollRevealOptions.js
@@ -1,12 +1,9 @@
 const ScrollRevealOptions = {
   reset: true,
-  duration: 1000,
-  rotate: {
-    x: 180,
-    y: 180,
-    z: 180,
-  },
-  scale: 0,
+  origin: "bottom",
+  distance: "30px",
+  duration: 700,
+  easing: "ease-in",
 };
 
 export { ScrollRevealOptions };
