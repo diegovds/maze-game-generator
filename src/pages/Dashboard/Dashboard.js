7b4425077ee7f2954c889907b7a206d4d9e88700
Diego Viana <dsantos@inf.ufsm.br>
2022-07-16 19:19:28 -0300

    showByUid in dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,55 +1,48 @@
-import styles from './Dashboard.module.css'
+import styles from "./Dashboard.module.css";
 
-import { Link } from 'react-router-dom'
-import { backend } from '../../backend/config'
+import { Link } from "react-router-dom";
+import { backend } from "../../backend/config";
 
 // components
-import MazeDelete from "../../components/MazeDelete"
+import MazeDelete from "../../components/MazeDelete";
 
 // hooks
-import { useAuthValue } from "../../context/AuthContext"
-import { useCallback, useEffect, useState } from 'react'
+import { useAuthValue } from "../../context/AuthContext";
+import { useCallback, useEffect, useState } from "react";
 
 const Dashboard = () => {
-  const {user} = useAuthValue()
-  const uid = user.uid
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-  const [userData, setUserData] = useState(undefined)
-  
-  const loadingUser = userData === undefined
+  const [userData, setUserData] = useState(undefined);
 
-  const searchUserData = useCallback ( async () => {
-      var response = await fetch(backend + "/users")
-      var data = await response.json()
-      data = data.data
+  const loadingUser = userData === undefined;
 
-      for(var user in data){
-        if(data[user].uid === uid){
+  const searchUserData = useCallback(async () => {
+    var response = await fetch(backend + "/users/" + uid + "/" + uid);
+    var data = await response.json();
+    data = data.data;
 
-          data[user].mazes.forEach(item => {
-            if (item.name.length > 8) {
-              item.name = item.name.substr(0,8)
-              item.name = item.name.concat("...")
-            }
-
-            item.created_at = new Date(item.created_at).toLocaleDateString('pt-BR')
-          })
-
-          setUserData(data[user])
-          /*console.log(data[user].mazes.length)*/
-        }
+    data.mazes.forEach((item) => {
+      if (item.name.length > 8) {
+        item.name = item.name.substr(0, 8);
+        item.name = item.name.concat("...");
       }
-    },
-    [uid]
-  )
+
+      item.created_at = new Date(item.created_at).toLocaleDateString("pt-BR");
+    });
+
+    setUserData(data);
+    /*console.log(data[user].mazes.length)*/
+  }, [uid]);
 
   useEffect(() => {
-    searchUserData()
-  }, [searchUserData])
+    searchUserData();
+  }, [searchUserData]);
 
   const reload = () => {
-    setUserData(undefined)
-    searchUserData()
+    setUserData(undefined);
+    searchUserData();
   };
 
   if (loadingUser) {
@@ -60,34 +53,40 @@ const Dashboard = () => {
           <p>Carregando...</p>
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <>
-        <div className={styles.dashboard}>
-          <h2>Dashboard</h2>
-          <p>Gerencie os seus jogos</p>
-          {userData && userData.mazes.length === 0 ? (
-            <div className={styles.nomazes}>
-              <br />
-              <p>Não foram encontrados jogos</p>
-              <Link to="/mazes/create" className="btn">
-                Criar primeiro jogo
-              </Link>
-            </div>
-          ) : (
-            <div className={styles.maze_header}>
-              {/*<span>Nome(s)</span>
+      <div className={styles.dashboard}>
+        <h2>Dashboard</h2>
+        <p>Gerencie os seus jogos</p>
+        {userData && userData.mazes.length === 0 ? (
+          <div className={styles.nomazes}>
+            <br />
+            <p>Não foram encontrados jogos</p>
+            <Link to="/mazes/create" className="btn">
+              Criar primeiro jogo
+            </Link>
+          </div>
+        ) : (
+          <div className={styles.maze_header}>
+            {/*<span>Nome(s)</span>
             <span>Ações</span>*/}
-            </div>
-          )}
-        </div>
-        <div className={styles.mazes_container}>
-          {userData &&
-            userData.mazes.map((userData) => <MazeDelete key={userData.id} maze={userData} childToParent={reload} />)}
-        </div>
-        {/*userData &&
+          </div>
+        )}
+      </div>
+      <div className={styles.mazes_container}>
+        {userData &&
+          userData.mazes.map((userData) => (
+            <MazeDelete
+              key={userData.id}
+              maze={userData}
+              childToParent={reload}
+            />
+          ))}
+      </div>
+      {/*userData &&
           userData.mazes.slice(0).reverse().map((userData) => (
             <div className={styles.maze_row} key={userData.id}>
               <p>{userData.name}</p>
@@ -112,8 +111,7 @@ const Dashboard = () => {
             </div>
           ))
       */}
-      </>
-    
+    </>
   );
 };
 
