34249ae4ba259d5a1419a003cf3c4e3d509809b4
Diego Viana <dsantos@inf.ufsm.br>
2022-07-16 19:12:18 -0300

    showByUid in maze create page

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -1,33 +1,28 @@
-import styles from './CreateMaze.module.css'
+import styles from "./CreateMaze.module.css";
 
-import { backend } from '../../backend/config'
-import { useAuthValue } from "../../context/AuthContext"
-import { useEffect, useState } from 'react'
+import { backend } from "../../backend/config";
+import { useAuthValue } from "../../context/AuthContext";
+import { useEffect, useState } from "react";
 
 const CreateMaze = () => {
-  const {user} = useAuthValue()
-  const uid = user.uid
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-  const [userId, setUserId] = useState(undefined)
+  const [userId, setUserId] = useState(undefined);
 
-  const loadingUser = userId === undefined
+  const loadingUser = userId === undefined;
 
   useEffect(() => {
     const searchUserId = async () => {
-      const response = await fetch(backend + "/users")
-      var data = await response.json()
-      data = data.data
-  
-      for(var user in data){
-        if(data[user].uid === uid){
-          setUserId(data[user].id)
-          /*console.log(data[user].id)*/
-        }
-      }
+      const response = await fetch(backend + "/users/" + uid + "/" + uid);
+      var data = await response.json();
+      data = data.data;
+
+      setUserId(data.id);
     };
-    searchUserId()
-  }, [uid])
-  
+    searchUserId();
+  }, [uid]);
+
   if (loadingUser) {
     return (
       <div className="loading">
@@ -36,19 +31,31 @@ const CreateMaze = () => {
           <p>Carregando...</p>
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <div className={styles.maze_container}>
       <div className={styles.create_maze}>
         <h2>Criar jogo</h2>
-        <p>Ao clicar no botão abaixo você será redirecionado para a página de criação do jogo.</p>
-        <a className="btn" target="_blank" rel="noopener noreferrer" href={"https://mazegamebuilder.vercel.app/index.html?userId=" + userId}>Ir para o Maze Builder</a>
+        <p>
+          Ao clicar no botão abaixo você será redirecionado para a página de
+          criação do jogo.
+        </p>
+        <a
+          className="btn"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={
+            "https://mazegamebuilder.vercel.app/index.html?userId=" + userId
+          }
+        >
+          Ir para o Maze Builder
+        </a>
         {/*<a className="btn" href="https://mazegamebuilder.vercel.app/index.html?esteDeenvio">Ir</a>*/}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateMaze
+export default CreateMaze;
