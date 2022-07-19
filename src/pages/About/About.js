4e666a06711cdb1e8f4dd2f949ec5d387af2482b
Diego Viana <dsantos@inf.ufsm.br>
2022-07-19 13:11:42 -0300

    update about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,20 +1,47 @@
-import styles from './About.module.css'
+import styles from "./About.module.css";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const About = () => {
-
   return (
     <div className={styles.about}>
-      <h2>
-        Sobre o My <span>Blockly Maze</span>
-      </h2>
-      <p>Descrição da aplicação...........</p>
+      <h2>Sobre:</h2>
+
+      <div className={styles.container}>
+        <section>
+          <h3>My Blockly Maze</h3>
+          <p className={styles.p_int}>
+            É uma plataforma de criação e compartilhamento de jogos de
+            labirinto, nela os usuários também podem jogar suas criações e as da
+            comunidade.
+          </p>
+          <p>
+            Os jogos utilizam programação baseada em blocos para concluir os
+            desafios.
+          </p>
+        </section>
+
+        <section>
+          <h3>Maze Builder</h3>
+          <p>
+            É a ferramenta de criação dos jogos, nela é possível elaborar um
+            jogo de labirinto com imagem de fundo e níveis de desafio
+            personalizados.
+          </p>
+        </section>
+        <section>
+          <h3>Maze Game</h3>
+          <p>
+            Uma versão modificada do Maze Blockly Games desenvolvido pelo
+            Google. Tem como função executar os jogos personalizados.
+          </p>
+        </section>
+      </div>
       <Link to="/mazes/create" className="btn">
         Criar jogo
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
