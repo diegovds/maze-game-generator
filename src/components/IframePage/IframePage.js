8da5d3b3de94864b3d7c1f688bb36fd7c2774ac3
Diego Viana <dsantos@inf.ufsm.br>
2022-09-02 15:56:10 -0300

    Update IframePage.js

--- src/components/IframePage/IframePage.js
+++ src/components/IframePage/IframePage.js
@@ -1,6 +1,33 @@
 import styles from "./IframePage.module.css";
 
-const IframePage = ({ link }) => {
+import { useAuthValue } from "../../context/AuthContext";
+import { useEffect, useCallback, useRef } from "react";
+import { backend } from "../../backend/config";
+
+const IframePage = ({ link, returnDataChildToParent }) => {
+  const isFirstRender = useRef(true);
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  const searchUserData = useCallback(async () => {
+    var response = await fetch(backend + "/users/" + uid + "/" + uid);
+    var data = await response.json();
+    data = data.data;
+
+    returnDataChildToParent(data.mazes[0].id);
+  }, [uid, returnDataChildToParent]);
+
+  useEffect(() => {}, [searchUserData]);
+
+  const reload = () => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    searchUserData();
+  };
+
   return (
     <div className={styles.container}>
       <iframe
@@ -8,6 +35,7 @@ const IframePage = ({ link }) => {
         className={styles.iframe}
         title="iframePage"
         frameBorder="0"
+        onLoad={reload}
       ></iframe>
     </div>
   );
