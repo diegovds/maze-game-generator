72d1ab1ce9205dca5aeea58fd96f207adf236b4b
Diego Viana <dsantos@inf.ufsm.br>
2022-11-29 17:43:35 -0300

    uso de useQuery em CreateMaze.js

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -1,45 +1,54 @@
 //import styles from "./CreateMaze.module.css";
 
-import { backend } from "../../backend/config";
+import { useQuery } from "react-query";
+import axio from "../../services/api";
 import { useAuthValue } from "../../context/AuthContext";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "../../components/Loading/Loading";
 import IframePage from "../../components/IframePage/IframePage";
+import LoadingError from "../../components/LoadingError/LoadingError";
 
 const CreateMaze = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
   const navigate = useNavigate();
 
-  const [userId, setUserId] = useState(undefined);
-
-  const loadingUser = userId === undefined;
-
   useEffect(() => {
-    const searchUserId = async () => {
-      const response = await fetch(backend + "/users/" + uid);
-      var data = await response.json();
-      data = data.data;
-
-      setUserId(data.id);
-    };
     document.title = "My BLOCKLY Maze | Criação";
-    searchUserId();
-  }, [uid]);
+  }, []);
+
+  const {
+    isLoading,
+    error,
+    data: userData,
+  } = useQuery(
+    ["user"],
+    async () =>
+      await axio.get("/users/" + uid).then((data) => {
+        data = data.data.data;
+
+        return data;
+      }),
+    {
+      staleTime: 1000 * 60, // só faz outra requisição depois de 1 minuto
+    }
+  );
 
   const redirect = (data) => {
     return navigate("/mazes/" + data);
   };
 
-  if (loadingUser) {
-    return <Loading />;
-  }
+  if (isLoading) return <Loading />;
+
+  if (error) return <LoadingError message={error.message} />;
 
   return (
     <>
       <IframePage
-        link={"https://mazegamebuilder.vercel.app/index.html?userId=" + userId}
+        link={
+          "https://mazegamebuilder.vercel.app/index.html?userId=" + userData.id
+        }
         redirect={redirect}
       />
     </>
