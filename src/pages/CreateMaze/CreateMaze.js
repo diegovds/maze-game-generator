5c76c96bb43ac695355db4eb5745473a3ad3e87c
Diego Viana <dsantos@inf.ufsm.br>
2022-11-29 17:59:23 -0300

    Revert "uso de useQuery em CreateMaze.js"
    This reverts commit 72d1ab1ce9205dca5aeea58fd96f207adf236b4b.

--- src/pages/CreateMaze/CreateMaze.js
+++ src/pages/CreateMaze/CreateMaze.js
@@ -1,54 +1,45 @@
 //import styles from "./CreateMaze.module.css";
 
-import { useQuery } from "react-query";
-import axio from "../../services/api";
+import { backend } from "../../backend/config";
 import { useAuthValue } from "../../context/AuthContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "../../components/Loading/Loading";
 import IframePage from "../../components/IframePage/IframePage";
-import LoadingError from "../../components/LoadingError/LoadingError";
 
 const CreateMaze = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
   const navigate = useNavigate();
 
+  const [userId, setUserId] = useState(undefined);
+
+  const loadingUser = userId === undefined;
+
   useEffect(() => {
+    const searchUserId = async () => {
+      const response = await fetch(backend + "/users/" + uid);
+      var data = await response.json();
+      data = data.data;
+
+      setUserId(data.id);
+    };
     document.title = "My BLOCKLY Maze | Criação";
-  }, []);
-
-  const {
-    isLoading,
-    error,
-    data: userData,
-  } = useQuery(
-    ["user"],
-    async () =>
-      await axio.get("/users/" + uid).then((data) => {
-        data = data.data.data;
-
-        return data;
-      }),
-    {
-      staleTime: 1000 * 60, // só faz outra requisição depois de 1 minuto
-    }
-  );
+    searchUserId();
+  }, [uid]);
 
   const redirect = (data) => {
     return navigate("/mazes/" + data);
   };
 
-  if (isLoading) return <Loading />;
-
-  if (error) return <LoadingError message={error.message} />;
+  if (loadingUser) {
+    return <Loading />;
+  }
 
   return (
     <>
       <IframePage
-        link={
-          "https://mazegamebuilder.vercel.app/index.html?userId=" + userData.id
-        }
+        link={"https://mazegamebuilder.vercel.app/index.html?userId=" + userId}
         redirect={redirect}
       />
     </>
