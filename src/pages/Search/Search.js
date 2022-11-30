925dfc2c263aa2856e460fdb4717577f64a7136a
Diego Viana <dsantos@inf.ufsm.br>
2022-11-30 18:44:00 -0300

    data fetch with axios in Search.js

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,6 +1,6 @@
 import styles from "./Search.module.css";
 
-import { backend } from "../../backend/config";
+import api from "../../services/api";
 
 // hook
 import { useState, useEffect } from "react";
@@ -9,53 +9,69 @@ import { useQuery } from "../../hooks/useQuery";
 // components
 import MazeDetail from "../../components/MazeDetail/MazeDetail";
 import Loading from "../../components/Loading/Loading";
+import LoadingError from "../../components/LoadingError/LoadingError";
 
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
 
-  const [mazes, setMazes] = useState(undefined);
-  const loadingMazes = mazes === undefined;
+  const [mazes, setMazes] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getFilterMazes = async () => {
-      setMazes(undefined);
-      const response = await fetch(backend + "/mazes");
-      var data = await response.json();
       var filter = [];
+      setIsFetching(true);
+      setMazes(null);
 
-      data = data.data;
+      await api
+        .get("/mazes")
+        .then((data) => {
+          data = data.data.data;
 
-      data.forEach((item) => {
-        if (item.name.toLowerCase().includes(search.toLowerCase())) {
-          filter.push(item);
-        }
+          data.forEach((item) => {
+            if (item.name.toLowerCase().includes(search.toLowerCase())) {
+              filter.push(item);
+            }
 
-        if (item.code !== null) {
-          if (
-            item.code.toLowerCase().includes(search.toLowerCase()) &&
-            filter.find((filtered) => filtered.id === item.id) === undefined
-          ) {
-            filter.push(item);
-          }
-        }
+            if (item.code !== null) {
+              if (
+                item.code.toLowerCase().includes(search.toLowerCase()) &&
+                filter.find((filtered) => filtered.id === item.id) === undefined
+              ) {
+                filter.push(item);
+              }
+            }
 
-        if (item.name.length > 8) {
-          item.name = item.name.substr(0, 8);
-          item.name = item.name.concat("...");
-        }
+            if (item.name.length > 8) {
+              item.name = item.name.substr(0, 8);
+              item.name = item.name.concat("...");
+            }
 
-        item.created_at = new Date(item.created_at).toLocaleDateString("pt-BR");
-      });
-      setMazes(filter);
+            item.created_at = new Date(item.created_at).toLocaleDateString(
+              "pt-BR"
+            );
+          });
+          setMazes(filter);
+        })
+        .catch((err) => {
+          setError(err);
+        })
+        .finally(() => {
+          setIsFetching(false);
+        });
     };
     document.title = "My BLOCKLY Maze | Busca";
     getFilterMazes();
-    /*console.log(filter)*/
   }, [search]);
 
-  if (loadingMazes) {
+  if (isFetching) {
     return <Loading />;
+  }
+
+  if (error) {
+    return <LoadingError message={error.message} />;
   }
 
   return (
@@ -68,18 +84,12 @@ const Search = () => {
               <p className={styles.p_a}>
                 Não foram encontrados jogos a partir da sua pesquisa...
               </p>
-              {/*<Link to="/" className="btn btn-dark">Voltar</Link>*/}
             </>
           )}
         </div>
       </div>
       <div className={styles.mazes_container}>
         {mazes && mazes.map((maze) => <MazeDetail key={maze.id} maze={maze} />)}
-      </div>
-      <div>
-        {mazes && mazes.length !== 0 && (
-          <>{/*<Link to="/" className="btn btn-dark">Voltar</Link>*/}</>
-        )}
       </div>
     </>
   );
