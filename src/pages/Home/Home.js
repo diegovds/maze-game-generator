9231c0b31d338754ab07ae5a4670f4e47fc14cc2
Diego Viana <dsantos@inf.ufsm.br>
2022-11-30 02:12:14 -0300

    Revert "uso de useQuery em Home.js"
    This reverts commit b7053caa56aeb0859db1c9579a7ba26b282cf011.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,7 @@
 import styles from "./Home.module.css";
 
-import { useQuery } from "react-query";
-import axio from "../../services/api";
-import { useEffect } from "react";
+import { backend } from "../../backend/config";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 // components
@@ -11,39 +10,49 @@ import Loading from "../../components/Loading/Loading";
 import LoadingError from "../../components/LoadingError/LoadingError";
 
 const Home = () => {
+  const [mazes, setMazes] = useState(undefined);
+  const [error, setError] = useState(undefined);
+
+  const loadingMazes = mazes === undefined;
+  const loadingError = error === undefined;
+
   useEffect(() => {
+    const getAllMazes = async () => {
+      fetch(backend + "/mazes")
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Ocorreu um erro, por favor tente mais tarde.");
+        })
+        .then((data) => {
+          data = data.data;
+
+          data.forEach((item) => {
+            if (item.name.length > 8) {
+              item.name = item.name.substr(0, 8);
+              item.name = item.name.concat("...");
+            }
+            item.created_at = new Date(item.created_at).toLocaleDateString(
+              "pt-BR"
+            );
+          });
+
+          setMazes(data);
+        })
+        .catch((error) => {
+          setError(error);
+        });
+    };
     document.title = "My BLOCKLY Maze | Home";
+    getAllMazes();
   }, []);
 
-  const {
-    isLoading,
-    error,
-    data: mazes,
-  } = useQuery(
-    ["mazes"],
-    async () =>
-      await axio.get("/mazes").then((data) => {
-        data = data.data.data;
-
-        data.forEach((item) => {
-          if (item.name.length > 8) {
-            item.name = item.name.substr(0, 8);
-            item.name = item.name.concat("...");
-          }
-          item.created_at = new Date(item.created_at).toLocaleDateString(
-            "pt-BR"
-          );
-        });
-
-        return data;
-      })
-  );
-
-  if (isLoading) {
+  if (loadingMazes && loadingError) {
     return <Loading />;
   }
 
-  if (error) {
+  if (!loadingError) {
     return <LoadingError message={error.message} />;
   }
 
