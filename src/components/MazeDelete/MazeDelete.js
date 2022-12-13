7d642ff39cfd27f7c4b098e0e2180fb60351f2bb
Diego Viana <dsantos@inf.ufsm.br>
2022-12-13 20:31:15 -0300

    Revert "adding image loading placeholder"
    This reverts commit a0ebb7f04913ed17de14ccf949828a498b8b2c60.

--- src/components/MazeDelete/MazeDelete.js
+++ src/components/MazeDelete/MazeDelete.js
@@ -65,7 +65,7 @@ const MazeDelete = ({ maze, returnDataChildToParent }) => {
         <div ref={elementRef}>
           <div className={styles.maze}>
             {/*<img src={maze.url_image} alt={maze.image} />*/}
-            <LazyLoadImage src={maze.url_image} placeholderSrc="./placeholder.png" effect="blur" alt={maze.image} />
+            <LazyLoadImage src={maze.url_image} effect="blur" alt={maze.image} />
             <h3>{maze.name}</h3>
             <p id="date">Criado em:<br />{maze.created_at}</p>
             <Link to={`/mazes/${maze.id}`} className="btn">
