43dfbbd96e696ba664153d455319b39b5d9a6172
Diego Viana <dsantos@inf.ufsm.br>
2022-12-13 20:31:30 -0300

    Revert "adding image loading animation"
    This reverts commit 98d4273203d89a5990c8b6ba3acc41a7138b8a92.

--- src/components/MazeDetail/MazeDetail.js
+++ src/components/MazeDetail/MazeDetail.js
@@ -6,9 +6,6 @@ import { useRef, useEffect } from 'react'
 
 import ScrollReveal from 'scrollreveal'
 
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
-
 import { ScrollRevealOptions } from "../Scroll/ScrollRevealOptions"
 
 const MazeDetail = ({ maze }) => {
@@ -24,8 +21,7 @@ const MazeDetail = ({ maze }) => {
     <div className='load-hidden'>
       <div  ref={elementRef}>
         <div className={styles.maze}>
-          {/*<img src={maze.url_image} alt={maze.image} />*/}
-          <LazyLoadImage src={maze.url_image} effect="blur" alt={maze.image} />
+          <img src={maze.url_image} alt={maze.image} />
           <h3>{maze.name}</h3>
           <p id='date'>Criado em:<br />{maze.created_at}</p>
           <Link to={`/mazes/${maze.id}`} className='btn'>Detalhes</Link>
