8b38f31caff56868d5444c9075cc1e356e40eab8
Diego Viana <dsantos@inf.ufsm.br>
2022-12-28 20:37:22 -0300

    handling image loading on homepage

--- src/components/MazeDetail/MazeDetail.js
+++ src/components/MazeDetail/MazeDetail.js
@@ -11,8 +11,8 @@ import { ScrollRevealOptions } from "../Scroll/ScrollRevealOptions"
 const MazeDetail = ({ maze }) => {
   const elementRef = useRef();
 
-  const [styleImg, setStyleImg] = useState(styles.img_loading);
-  const [styleImgLoading, setStyleImgLoading] = useState(styles.img_loaded);
+  const [styleImg, setStyleImg] = useState("img_loading");
+  const [styleImgLoading, setStyleImgLoading] = useState('img_loaded');
 
   useEffect(() => {
     const divElement = elementRef.current;
@@ -20,16 +20,16 @@ const MazeDetail = ({ maze }) => {
   }, []);
 
   const imgChange = () => {
-    setStyleImg(styles.img_loaded)
-    setStyleImgLoading(styles.img_loading)
+    setStyleImg('img_loaded')
+    setStyleImgLoading('img_loading')
   };
 
   return (
     <div className='load-hidden'>
       <div  ref={elementRef}>
         <div className={styles.maze}>
-          <img className={styleImg} src={maze.url_image} alt={maze.image} onLoad={imgChange}/>
           <img className={styleImgLoading} src="./null.png" alt="Imagem de carregamento" />
+          <img className={styleImg} src={maze.url_image} alt={maze.image} onLoad={imgChange}/>
           <h3>{maze.name}</h3>
           <p id='date'>Criado em:<br />{maze.created_at}</p>
           <Link to={`/mazes/${maze.id}`} className='btn'>Detalhes</Link>
