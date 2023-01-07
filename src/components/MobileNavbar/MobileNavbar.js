6a7fd7cefde56b6471ea4002c93387bafd86bc09
Diego Viana <dsantos@inf.ufsm.br>
2023-01-07 18:54:35 -0300

    mobile navbar update

--- src/components/MobileNavbar/MobileNavbar.js
+++ src/components/MobileNavbar/MobileNavbar.js
@@ -1,135 +1,140 @@
 import styles from "./MobileNavbar.module.css";
 
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-
 import { HiSearch } from "react-icons/hi";
 import { FaPuzzlePiece } from "react-icons/fa";
 import { Squash as Hamburger } from "hamburger-react";
 
+import { NavLink } from "react-router-dom";
+
+import { useState } from "react";
 import { useAuthentication } from "../../hooks/useAuthentication";
+import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
 
-const NewNavbar = () => {
+const MobileNavbar = () => {
   const { user } = useAuthValue();
   const { logout } = useAuthentication();
+  const navigate = useNavigate();
 
-  const [showLinks, setShowLiks] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showMenu, setShowMenu] = useState(false);
   const [isOpen, setOpen] = useState(false);
 
   const changeHamburger = () => {
     if (isOpen) {
       setOpen(!isOpen);
-      setShowLiks(false);
+      setShowMenu(false);
     } else {
-      setShowLiks(true);
+      setShowMenu(true);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    if (query) {
+      return navigate(`/search?q=${query}`);
     }
   };
 
   return (
     <div className={styles.main}>
       <div className={styles.navbar}>
-        <div className={styles.leftSide}>
-          <div className={styles.links} id={showLinks ? styles.hidden : ""}>
-            <NavLink
-              to="/"
-              onClick={() => {
-                changeHamburger();
-              }}
-            >
-              Home
-            </NavLink>
-            {!user && (
-              <>
-                <NavLink
-                  to="/login"
-                  onClick={() => {
-                    changeHamburger();
-                  }}
-                >
-                  Entrar
-                </NavLink>
-                <NavLink
-                  to="/register"
-                  onClick={() => {
-                    changeHamburger();
-                  }}
-                >
-                  Cadastrar
-                </NavLink>
-              </>
-            )}
-            {user && (
-              <>
-                <NavLink
-                  to="/mazes/create"
-                  onClick={() => {
-                    changeHamburger();
-                  }}
-                >
-                  Criar novo jogo
-                </NavLink>
-                <NavLink
-                  to="/dashboard"
-                  onClick={() => {
-                    changeHamburger();
-                  }}
-                >
-                  Dashboard
-                </NavLink>
-              </>
-            )}
-
-            <NavLink
-              to="/about"
-              onClick={() => {
-                changeHamburger();
-              }}
-            >
-              Sobre
-            </NavLink>
-            {user && (
-              <>
-                <button
-                  className={styles.btn_logout}
-                  onClick={() => {
-                    logout();
-                    changeHamburger();
-                  }}
-                >
-                  Sair
-                </button>
-              </>
-            )}
-          </div>
-          {/*<button onClick={() => setShowLiks(!showLinks)}>
-            <IoMdReorder />
-          </button>*/}
-          <div
-            className={styles.hamburger}
+        <NavLink to="/" className={styles.brand}>
+          <FaPuzzlePiece />
+          My <span>Blockly</span> Maze
+        </NavLink>
+        <div className={styles.menu} id={showMenu ? styles.hidden : ""}>
+          
+          <NavLink
+            to="/"
             onClick={() => {
               changeHamburger();
             }}
           >
-            <Hamburger
-              size={22}
-              hideOutline={false}
-              rounded={true}
-              color="#000"
-              toggled={isOpen}
-              toggle={setOpen}
-            />
-          </div>
-        </div>
-        <div className={styles.rightSide}>
-          <NavLink to="/" className={styles.brand}>
-            <FaPuzzlePiece />
-            My <span>Blockly</span> Maze
+            Home
           </NavLink>
+          {!user && (
+            <>
+              <NavLink
+                to="/login"
+                onClick={() => {
+                  changeHamburger();
+                }}
+              >
+                Entrar
+              </NavLink>
+              <NavLink
+                to="/register"
+                onClick={() => {
+                  changeHamburger();
+                }}
+              >
+                Cadastrar
+              </NavLink>
+            </>
+          )}
+          {user && (
+            <>
+              <NavLink
+                to="/mazes/create"
+                onClick={() => {
+                  changeHamburger();
+                }}
+              >
+                Criar novo jogo
+              </NavLink>
+              <NavLink
+                to="/dashboard"
+                onClick={() => {
+                  changeHamburger();
+                }}
+              >
+                Dashboard
+              </NavLink>
+            </>
+          )}
+
+          <NavLink
+            to="/about"
+            onClick={() => {
+              changeHamburger();
+            }}
+          >
+            Sobre
+          </NavLink>
+          {user && (
+            <>
+              <button
+                className={styles.btn_logout}
+                onClick={() => {
+                  logout();
+                  changeHamburger();
+                }}
+              >
+                Sair
+              </button>
+            </>
+          )}
+        </div>
+        <div
+          className={styles.hamburger}
+          onClick={() => {
+            changeHamburger();
+          }}
+        >
+          <Hamburger
+            size={26}
+            hideOutline={false}
+            rounded={true}
+            color="#000"
+            toggled={isOpen}
+            toggle={setOpen}
+          />
         </div>
       </div>
     </div>
   );
 };
 
-export default NewNavbar;
+export default MobileNavbar;
