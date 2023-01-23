d281e67e04357509d523c5fc1df7d6b75051b203
Diego Viana <dsantos@inf.ufsm.br>
2023-01-23 20:05:20 -0300

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,31 @@
 import styles from "./About.module.css";
 
+import { motion } from "framer-motion";
+
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 
 const About = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
   useEffect(() => {
     document.title = "My BLOCKLY Maze | Sobre";
   }, []);
@@ -12,8 +34,13 @@ const About = () => {
     <div className={styles.about}>
       <h2>Sobre:</h2>
 
-      <div className={styles.container}>
-        <section>
+      <motion.div
+        className={styles.container}
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.section variants={item}>
           <h3>My Blockly Maze</h3>
           <p className={styles.p_int}>
             É uma plataforma de criação e compartilhamento de jogos de
@@ -23,24 +50,24 @@ const About = () => {
             Os jogos utilizam programação baseada em blocos para concluir os
             desafios.
           </p>
-        </section>
+        </motion.section>
 
-        <section>
+        <motion.section variants={item}>
           <h3>Maze Builder</h3>
           <p className={styles.p_int}>
             É a ferramenta de criação dos jogos, nela é possível elaborar um
             jogo de labirinto com imagem de fundo e níveis de desafio
             personalizados.
           </p>
-        </section>
-        <section>
+        </motion.section>
+        <motion.section variants={item}>
           <h3>Maze Game</h3>
           <p className={styles.p_int}>
             Uma versão modificada do Maze Blockly Games desenvolvido pelo
             Google. Tem como função executar os jogos personalizados.
           </p>
-        </section>
-      </div>
+        </motion.section>
+      </motion.div>
       <Link to="/mazes/create" className="btn">
         Criar jogo
       </Link>
