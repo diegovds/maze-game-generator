52eb0459a9b808ecb1af2781052cbd0d292e8dbd
Diego Viana <dsantos@inf.ufsm.br>
2023-01-23 00:13:24 -0300

    implementing password recovery on login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -8,6 +8,8 @@ import { isEmail } from "validator";
 
 const Login = () => {
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [recoverPassword, setRecoverPassword] = useState(false);
 
   const {
     register,
@@ -15,19 +17,26 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-  const { login, error: authError, loading } = useAuthentication();
+  const {
+    login,
+    forgotPassword,
+    error: authError,
+    success: emailSent,
+    loading,
+  } = useAuthentication();
 
   const submit = async (data) => {
     const { email, password } = data;
 
     setError("");
+    setSuccess("");
 
     const user = {
       email,
       password,
     };
 
-    await login(user);
+    !recoverPassword ? await login(user) : await forgotPassword(user.email);
   };
 
   useEffect(() => {
@@ -35,13 +44,26 @@ const Login = () => {
     if (authError) {
       setError(authError);
     }
-  }, [authError]);
+
+    if (emailSent) {
+      setSuccess(emailSent);
+    }
+  }, [authError, emailSent]);
 
   return (
     <div className={styles.container}>
       <div className={styles.login}>
-        <h2>Entrar</h2>
-        <p>Insira suas credenciais</p>
+        {!recoverPassword ? (
+          <>
+            <h2>Entrar</h2>
+            <p>Insira suas credenciais</p>
+          </>
+        ) : (
+          <>
+            <h2>Redefinição de senha</h2>
+            <p>Insira o e-mail cadastrado</p>
+          </>
+        )}
         <form onSubmit={handleSubmit(submit)} className={styles.form}>
           <input
             type="text"
@@ -60,11 +82,13 @@ const Login = () => {
               O e-mail informado não é válido.
             </p>
           )}
-          <input
-            type="password"
-            placeholder="Senha"
-            {...register("password", { required: true, minLength: 6 })}
-          />
+          {!recoverPassword && (
+            <input
+              type="password"
+              placeholder="Senha"
+              {...register("password", { required: true, minLength: 6 })}
+            />
+          )}
           {errors?.password?.type === "minLength" && (
             <p className={styles.inputError}>
               A senha precisa conter pelo menos 6 caracteres.
@@ -73,13 +97,28 @@ const Login = () => {
           {errors?.password?.type === "required" && (
             <p className={styles.inputError}>A senha precisa ser informada.</p>
           )}
-          {!loading && <button className="btn">Entrar</button>}
+          {!loading && !recoverPassword && (
+            <button className="btn">Entrar</button>
+          )}
+          {!loading && recoverPassword && (
+            <button className="btn">Redefinir senha</button>
+          )}
           {loading && (
             <button className="btn" disabled>
               Aguarde...
             </button>
           )}
+          {!recoverPassword ? (
+            <button className={styles.recoverButton} onClick={() => setRecoverPassword(true)}>
+              Esqueci minha senha
+            </button>
+          ) : (
+            <button className={styles.recoverButton} onClick={() => setRecoverPassword(false)}>
+              Voltar
+            </button>
+          )}
           {error && <p className="error">{error}</p>}
+          {success && <p className="success">{success}</p>}
         </form>
       </div>
     </div>
