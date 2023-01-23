7f3e6f56818da6d1e037958e96acf4c1ef7ae753
Diego Viana <dsantos@inf.ufsm.br>
2023-01-23 19:23:07 -0300

    Update FramerMotionOptions.js

--- src/components/FramerMotionOptions.js
+++ src/components/FramerMotionOptions.js
@@ -1,7 +1,7 @@
-const initial = { opacity: 0, scale: 0.95 };
+const initial = { opacity: 0, scale: 0.5 };
 
 const whileInView = { opacity: 1, scale: 1 };
 
-const transition = { duration: 0.4, ease: "linear" };
+const transition = { duration: 0.8, delay: 0.25, ease: [0, 0.71, 0.2, 1.01] };
 
 export { initial, whileInView, transition };
