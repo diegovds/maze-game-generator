5b6c8e0d6bf2019b955daa7aac8a3d8831dd1b75
Diego Viana <dsantos@inf.ufsm.br>
2023-01-23 21:22:46 -0300

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,31 +1,12 @@
 import styles from "./About.module.css";
 
 import { motion } from "framer-motion";
+import { container, item } from "../../components/FramerMotionOptions";
 
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 
 const About = () => {
-  const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delayChildren: 0.3,
-        staggerChildren: 0.2,
-      },
-    },
-  };
-
-  const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-      y: 0,
-      opacity: 1,
-    },
-  };
-
   useEffect(() => {
     document.title = "My BLOCKLY Maze | Sobre";
   }, []);
