9472dc28b49dadca44d3b67ea43f6b9e87056561
Diego Viana <dsantos@inf.ufsm.br>
2023-01-23 21:22:32 -0300

    Update FramerMotionOptions.js

--- src/components/FramerMotionOptions.js
+++ src/components/FramerMotionOptions.js
@@ -4,4 +4,48 @@ const whileInView = { opacity: 1, scale: 1 };
 
 const transition = { duration: 0.8, delay: 0.25, ease: [0, 0.71, 0.2, 1.01] };
 
-export { initial, whileInView, transition };
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 0, opacity: 0 }, /** ou y:20 */
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+const leftItem = {
+  hidden: { x: -20, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+
+const rightItem = {
+  hidden: { x: 20, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+
+export {
+  initial,
+  whileInView,
+  transition,
+  container,
+  item,
+  leftItem,
+  rightItem,
+};
