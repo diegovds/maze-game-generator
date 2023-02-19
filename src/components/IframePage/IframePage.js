439ed33b8f5d038aca6e289407785a0b6ac8adf1
Diego Viana <dsantos@inf.ufsm.br>
2023-02-19 18:16:50 -0300

    removing `message` event when component is unmounted

--- src/components/IframePage/IframePage.js
+++ src/components/IframePage/IframePage.js
@@ -1,6 +1,6 @@
 import styles from "./IframePage.module.css";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback } from "react";
 
 const IframePage = ({ link, redirect }) => {
   const iframe = useRef();
@@ -9,22 +9,33 @@ const IframePage = ({ link, redirect }) => {
     iframe.current.scrollIntoView();
   }, []);
 
-  // window escutando o evento `message` que o postMessage envia
-  window.addEventListener("message", function (e) {
-    // para ter acesso ao dado temos que acessar a propriedade data
-    //if (e.data === "mensagem vinda do iframe") {
-    if (String(e.data).includes("mensagem")) {
-      //console.log(e.data);
-      if (String(e.data).includes("mazeId=")) {
-        //console.log("tem mazeId");
-        let mazeId = String(e.data).slice(17);
-        //console.log(mazeId); /** id do novo maze criado */
-        redirect(mazeId);
-      } else {
-        redirect();
+  const handleMessage = useCallback(
+    (e) => {
+      // para ter acesso ao dado temos que acessar a propriedade data
+      //if (e.data === "mensagem vinda do iframe") {
+      if (String(e.data).includes("mensagem")) {
+        //console.log(e.data);
+        if (String(e.data).includes("mazeId=")) {
+          //console.log("tem mazeId");
+          let mazeId = String(e.data).slice(17);
+          //console.log(mazeId); /** id do novo maze criado */
+          redirect(mazeId);
+        } else {
+          redirect();
+        }
       }
-    }
-  });
+    },
+    [redirect]
+  );
+
+  useEffect(() => {
+    // window escutando o evento `message` que o postMessage envia
+    window.addEventListener("message", handleMessage);
+    // 👇️ remove o event listener quando o componente é desmontado
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [handleMessage]);
 
   return (
     <div className={styles.container}>
