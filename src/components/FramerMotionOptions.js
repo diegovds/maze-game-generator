69b2873b54cdb98386cd29076555bb38f86a527a
Diego Viana <dsantos@inf.ufsm.br>
2023-02-23 22:16:27 -0300

    Update FramerMotionOptions.js

--- src/components/FramerMotionOptions.js
+++ src/components/FramerMotionOptions.js
@@ -9,37 +9,38 @@ const transition = {
 };
 
 const cardAnimate = {
-  hidden: { opacity: 0, y: 100 },
+  hidden: { opacity: 0, y: "100%" },
   visible: {
     opacity: 1,
     y: 0,
     transition: {
       type: "spring",
-      bounce: 0.4,
-      duration: 1,
-      delayChildren: 0.3,
+      bounce: 0.2,
+      duration: 1.75,
+      delay: 0.25,
+      delayChildren: 0.75,
       staggerChildren: 0.5,
     },
   },
 };
 
 const imageAnimate = {
-  hidden: { x: -100, opacity: 0 },
+  hidden: { x: "-100%", opacity: 0 },
   visible: {
     x: 0,
     opacity: 1,
     /*rotate: [0, 10, 0],*/
-    transition: { type: "spring", bounce: 0.4, duration: 1 },
+    transition: { type: "spring", bounce: 0.2, duration: 1.75 },
   },
 };
 
 const textAnimate = {
-  hidden: { x: 100, opacity: 0 },
+  hidden: { x: "100%", opacity: 0 },
   visible: {
     x: 0,
     opacity: 1,
-    rotate: [0, -10, 0],
-    transition: { type: "spring", bounce: 0.4, duration: 1 },
+    /*rotate: [0, -10, 0],*/
+    transition: { type: "spring", bounce: 0.2, duration: 1.75 },
   },
 };
 
