54d5d52af9398a00ab26270a059d96a47bb1cc77
Diego Viana <dsantos@inf.ufsm.br>
2023-02-26 17:11:20 -0300

    new reveal animation in MazeDelete component

--- src/components/MazeDelete/MazeDelete.js
+++ src/components/MazeDelete/MazeDelete.js
@@ -1,15 +1,16 @@
 import styles from "./MazeDelete.module.css";
 
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
-import { motion } from "framer-motion";
-import { initial, whileInView, transition } from "../FramerMotionOptions";
+import { useState, useRef } from "react";
+import { useIsVisible } from "../../hooks/useIsVisible";
 
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const MazeDelete = ({ maze, getMazeDelete, loadingDelete = false }) => {
+  const ref = useRef();
+  const isVisible = useIsVisible(ref);
+
   const [styleImg, setStyleImg] = useState("img_loading");
   const [skeleton, setSkeleton] = useState(true);
 
@@ -18,46 +19,47 @@ const MazeDelete = ({ maze, getMazeDelete, loadingDelete = false }) => {
   };
 
   return (
-    <motion.div
-      initial={initial}
-      whileInView={whileInView}
-      transition={transition}
-    >
-      <div className={styles.maze}>
-        {skeleton && <Skeleton width={`100%`} />}
-        <img
-          className={styleImg}
-          src={maze.url_image}
-          alt={maze.image}
-          onLoad={() => {
-            setSkeleton(false);
-            setStyleImg("img_loaded");
-          }}
-        />
-        <h3>{maze.name}</h3>
-        <p id="date">
-          Criado em:
-          <br />
-          {maze.created_at}
-        </p>
-        <Link to={`/mazes/${maze.id}`} className="btn">
-          Detalhes
-        </Link>
-        {!loadingDelete && (
-          <button
-            onClick={() => deleteMaze()}
-            className="btn btn-outline btn-danger"
-          >
-            Excluir
-          </button>
-        )}
-        {loadingDelete && (
-          <button className="btn" disabled>
-            Aguarde...
-          </button>
-        )}
+    <div className={styles.flip_card} ref={ref}>
+      <div
+        className={`${styles.flip_card_inner} ${isVisible ? styles.flip : ""}`}
+      >
+        <div className={styles.flip_card_front}></div>
+        <div className={styles.flip_card_back}>
+          {skeleton && <Skeleton width={`100%`} />}
+          <img
+            className={styleImg}
+            src={maze.url_image}
+            alt={maze.image}
+            onLoad={() => {
+              setSkeleton(false);
+              setStyleImg("img_loaded");
+            }}
+          />
+          <h3>{maze.name}</h3>
+          <p id="date">
+            Criado em:
+            <br />
+            {maze.created_at}
+          </p>
+          <Link to={`/mazes/${maze.id}`} className="btn">
+            Detalhes
+          </Link>
+          {!loadingDelete && (
+            <button
+              onClick={() => deleteMaze()}
+              className="btn btn-outline btn-danger"
+            >
+              Excluir
+            </button>
+          )}
+          {loadingDelete && (
+            <button className="btn" disabled>
+              Aguarde...
+            </button>
+          )}
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
