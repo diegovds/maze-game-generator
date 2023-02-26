33e0fa7c0c8f8c07b070bea733bb8150b6bcfb8f
Diego Viana <dsantos@inf.ufsm.br>
2023-02-26 16:19:17 -0300

    new reveal animation in MazeDetail component

--- src/components/MazeDetail/MazeDetail.js
+++ src/components/MazeDetail/MazeDetail.js
@@ -1,46 +1,48 @@
 import styles from "./MazeDetail.module.css";
 
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
-import { motion } from "framer-motion";
-import { initial, whileInView, transition } from "../FramerMotionOptions";
+import { useState, useRef } from "react";
+import { useIsVisible } from "../../hooks/useIsVisible";
 
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const MazeDetail = ({ maze }) => {
+  const ref = useRef();
+  const isVisible = useIsVisible(ref);
+
   const [styleImg, setStyleImg] = useState("img_loading");
   const [skeleton, setSkeleton] = useState(true);
 
   return (
-    <motion.div
-      initial={initial}
-      whileInView={whileInView}
-      transition={transition}
-    >
-      <div className={styles.maze}>
-        {skeleton && <Skeleton width={`100%`} />}
-        <img
-          className={styleImg}
-          src={maze.url_image}
-          alt={maze.image}
-          onLoad={() => {
-            setSkeleton(false);
-            setStyleImg("img_loaded");
-          }}
-        />
-        <h3>{maze.name}</h3>
-        <p id="date">
-          Criado em:
-          <br />
-          {maze.created_at}
-        </p>
-        <Link to={`/mazes/${maze.id}`} className="btn">
-          Detalhes
-        </Link>
+    <div className={styles.flip_card} ref={ref}>
+      <div
+        className={`${styles.flip_card_inner} ${isVisible ? styles.flip : ""}`}
+      >
+        <div className={styles.flip_card_front}></div>
+        <div className={styles.flip_card_back}>
+          {skeleton && <Skeleton width={`100%`} />}
+          <img
+            className={styleImg}
+            src={maze.url_image}
+            alt={maze.image}
+            onLoad={() => {
+              setSkeleton(false);
+              setStyleImg("img_loaded");
+            }}
+          />
+          <h3>{maze.name}</h3>
+          <p id="date">
+            Criado em:
+            <br />
+            {maze.created_at}
+          </p>
+          <Link to={`/mazes/${maze.id}`} className="btn">
+            Detalhes
+          </Link>
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
---
+++ src/hooks/useIsVisible.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export const useIsVisible = (ref) => {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting)
+    );
+
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+};
