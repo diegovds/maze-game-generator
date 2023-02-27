de7de597d5b52d24f624205693b876e412dd274a
Diego Viana <dsantos@inf.ufsm.br>
2023-02-26 22:14:05 -0300

    getting dynamic height from MazeDetail component

--- src/components/MazeDetail/MazeDetail.js
+++ src/components/MazeDetail/MazeDetail.js
@@ -3,6 +3,7 @@ import styles from "./MazeDetail.module.css";
 import { Link } from "react-router-dom";
 import { useState, useRef } from "react";
 import { useInView } from "framer-motion";
+import { useElementSize } from "usehooks-ts";
 
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -11,16 +12,21 @@ const MazeDetail = ({ maze }) => {
   const ref = useRef();
   const isVisible = useInView(ref);
 
+  const [refFlipCardBack, { height }] = useElementSize();
+
   const [styleImg, setStyleImg] = useState("img_loading");
   const [skeleton, setSkeleton] = useState(true);
 
   return (
-    <div className={styles.flip_card} ref={ref}>
+    <div className={styles.flip_card} ref={ref} style={{ height: height }}>
       <div
         className={`${styles.flip_card_inner} ${isVisible ? styles.flip : ""}`}
       >
-        <div className={styles.flip_card_front}></div>
-        <div className={styles.flip_card_back}>
+        <div
+          className={styles.flip_card_front}
+          style={{ height: height }}
+        ></div>
+        <div className={styles.flip_card_back} ref={refFlipCardBack}>
           {skeleton && <Skeleton width={`100%`} />}
           <img
             className={styleImg}
