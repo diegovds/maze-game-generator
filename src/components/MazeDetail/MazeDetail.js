2a38e8a347c4da7f0c97d05a09fdab93164dfe88
Diego Viana <dsantos@inf.ufsm.br>
2023-02-26 21:37:44 -0300

    using useInView to detect component in viewport

--- src/components/MazeDetail/MazeDetail.js
+++ src/components/MazeDetail/MazeDetail.js
@@ -2,14 +2,14 @@ import styles from "./MazeDetail.module.css";
 
 import { Link } from "react-router-dom";
 import { useState, useRef } from "react";
-import { useIsVisible } from "../../hooks/useIsVisible";
+import { useInView } from "framer-motion";
 
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const MazeDetail = ({ maze }) => {
   const ref = useRef();
-  const isVisible = useIsVisible(ref);
+  const isVisible = useInView(ref);
 
   const [styleImg, setStyleImg] = useState("img_loading");
   const [skeleton, setSkeleton] = useState(true);
